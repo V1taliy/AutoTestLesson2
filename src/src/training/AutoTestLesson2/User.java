d3e8c28beefefaa06dbfbb01959bdbfa215cfb7b
VitaliyS <biggone24@gmail.com>
2016-10-07 18:50:13 +0300

    1

--- src/src/training/AutoTestLesson2/User.java
+++ src/src/training/AutoTestLesson2/User.java
@@ -5,23 +5,18 @@ public class User {
     private String username;
     private int password;
 
+    public User() {
+        username = "TestUsername";  // == this.username = ...
+    }
+
     public User(User secondUser) {
         this.username = secondUser.username;
         this.password = secondUser.password;
     }
 
     public User(String username, int password) {
-        User secondUser = new User("TestUsername", 123456);
         this.username = username;
         this.password = password;
-
-
-    }
-
-    public User() {
-        User myUser = new User();
-        myUser.username = "TestUsername";
-
     }
 
     public String getUsername() {
@@ -40,8 +35,25 @@ public class User {
         this.password = password;
 
     }
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password=" + password +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        User user1 = new User("User1", 123);
+        User user2 = new User("User2", 321);
+
+        System.out.println(user2.toString());
+
+        user2 = new User(user1);
+
+        System.out.println(user2.toString());
+
     }
 }
--- src/test/java/AutoTestLesson2/UserTest.java
+++ src/test/java/AutoTestLesson2/UserTest.java
@@ -4,13 +4,33 @@ import org.junit.Assert;
 import org.testng.annotations.Test;
 
 
-public class UserTest{
+public class UserTest {
 
     @Test
-    public void testMyUser(){
-User asd = new User();
-        String username = asd.getUsername();
+    public void testMyUser() {
+        User testUser = new User();
+//        String username = testUser.getUsername();
+//        testUser.setUsername("TestUsername");
+        Assert.assertNotNull(testUser.getUsername());
+        Assert.assertEquals(testUser.getUsername(), "TestUsername");
+    }
 
-        Assert.assertNotNull(username);
-        Assert.assertEquals(username, "TestUsername") ;   }
-        }
+    @Test
+    public void testMySecondUser() {
+        User testUser = new User("qwerty", 123456);
+//        testUser.setUsername("qwerty");
+//        testUser.setPassword(123456);
+        Assert.assertEquals("qwerty", testUser.getUsername());
+        Assert.assertEquals(123456, testUser.getPassword());
+    }
+
+    @Test
+    public void testMyThirdUser() {
+        User user1 = new User("test", 123);
+        User user2 = new User(user1);
+        Assert.assertEquals(user1.getUsername(), user2.getUsername());
+        Assert.assertEquals(user1.getPassword(), user2.getPassword());
+    }
+
+
+}
