cb3b0dbed687c46309ba924ae1d57df383b591fc
Vitaliy <biggone24@gmail.com>
2016-10-12 02:01:45 +0300

    kk

--- src/test/java/AutoTestLesson2/UserTest.java
+++ src/test/java/AutoTestLesson2/UserTest.java
@@ -33,12 +33,12 @@ public class UserTest {
 @Test
     public void firstUserAverageSalary(){
 
-    User user1 = new User(142.64);
+    User user1 = new User("Gani.P", 2014, 53, 'M' , 142.64 , "Java developer", false , "TRUE" , "Sushi"  );
     User user2 = new User("Niru.X", 2015, 167, 'F' , 280 , "Byssines Analyst", true , "FALSE" , "Burger"  );
     User user3 = new User("Tony.A", 2016, 10, 'F' , 300.1 , "Web designer", false , "FALSE" , "Pizza" );
 
-    Assert.assertEquals(142.64, user1.getAveregeSalaryLastYear());
-   // Assert.assertEquals(user2.isHadPreviousJob(true), user2.isHadPreviousJob());
+    Assert.assertEquals(user1.getAveregeSalaryLastYear(), user2.getAveregeSalaryLastYear());
+    //Assert.assertEquals(user2.isHadPreviousJob(true), user2.isHadPreviousJob());
 
 }
 
